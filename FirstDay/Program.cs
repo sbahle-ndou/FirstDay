c5b00429e2ebb3491886203eb1819e648a3fb954
SBICZA01\A253563 <sbahle.ndou@standardbank.co.za>
2023-12-05 08:58:07 +0200

    Add project files.

---
+++ FirstDay/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //We are using Write not WriteLine
+            Console.WriteLine("Enter your name ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            Console.WriteLine("how old are you?");
+            //the values from a user are always string
+            String age = Console.ReadLine();
+            Console.WriteLine("you are" + " " + age);
+            Console.ReadLine();
+        }
+    }
+}
